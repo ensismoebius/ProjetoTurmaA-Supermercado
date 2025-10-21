b0acbce98d8a24594ee349429578a134a3df1279
laiscalafange <laiscalafange@gmail.com>
2025-10-21 15:42:05 -0300

    destruir sessao
    Implementei a lógica para destruir a sessão do usuário
    
    resolve #95

---
+++ src/View/index.php
@@ -0,0 +1,15 @@
+<?php
+require "vendor/autoload.php";
+
+const URL = "http://localhost";
+
+$roteador = new CoffeeCode\Router\Router(URL);
+
+$roteador->namespace("GrupoA\Supermercado\Controller");
+
+// === Área pública ===
+
+// === Área de login ===
+
+
+$roteador->get('/logout', "Login:logout");
