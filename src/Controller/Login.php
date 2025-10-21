f01a70040420b6415f1d26d4185832c895c92255
laiscalafange <laiscalafange@gmail.com>
2025-10-21 14:26:58 -0300

    criei login.php
    alterei a lógica para usar password verify e alterei a tabela para que tivesse espaço para a senha no hash
    
    resolve #82
    resolve #81

---
+++ src/Controller/Login.php
@@ -0,0 +1,86 @@
+<?php
+namespace GrupoA\Supermercado\Controller;
+
+use GrupoA\Supermercado\Database;
+use GrupoA\Supermercado\User;
+
+class Login {
+
+    // bla bla bla
+
+    public function __construct() {
+        session_start();
+
+        
+    }
+
+        /**
+     * Exibe o formulário de login
+     * @param array $dados
+     * @return void
+     */
+  
+     //colocar rotas
+
+    /**
+     * Autentica o usuário
+     * @param array $dados
+     * @return void
+     */
+    
+    function autenticar(array $dados)
+{
+    $email = trim($dados["email"]);
+    $senha = $dados["senha"];
+
+    $avisos = "";
+
+    if ($email == "" || $senha == "") {
+        $avisos .= "Preencha todos os campos.";
+    } else {
+        $bd = new Database();
+        $usuario = $bd->loadUserByEmail($email);
+
+        if ($usuario && password_verify($senha, $usuario["senha"])) {
+            $_SESSION["usuario"] = $usuario;
+            header("Location: /");
+            exit;
+        } else {
+            $avisos .= "Email ou senha inválidos.";
+        }
+    }
+
+    $dados["avisos"] = $avisos;
+    // mudar conforme criarem o html
+    echo $this->ambiente->render("formularioLogin.html", $dados);
+}
+
+
+//colocar rotas
+
+    /**
+     * Salva um novo login
+     * @param array $dados
+     * @return void
+     */
+    public function salvarLogin(array $dados)
+    {
+        //
+    }
+
+    public function logout(array $dados)
+    {
+        // Inicia a sessão
+        session_start();
+
+        // Remove o usuário da sessão
+        // unset no php é pra apagar um variavel 
+        unset($_SESSION["usuario"]);
+
+        // Redireciona para a página principal
+        header("Location: /");
+        exit;
+    }
+
+}
+
