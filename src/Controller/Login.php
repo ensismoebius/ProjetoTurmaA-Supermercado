914a03ea6c62abf682cb422dbf1af8c2976c7f09
André Furlan <ensismoebius@gmail.com>
2025-11-04 14:07:52 -0300

    Adicionada a classe de banco de dados

--- src/Controller/Login.php
+++ src/Controller/Login.php
@@ -7,29 +7,19 @@ use GrupoA\Supermercado\User;
 class Login
 {
 
-    // bla bla bla
+    private \Twig\Environment $ambiente;
+    private \Twig\Loader\FilesystemLoader $carregador;
 
     public function __construct()
     {
         session_start();
-
-
     }
-
-    /**
-     * Exibe o formulário de login
-     * @param array $dados
-     * @return void
-     */
-
-    //colocar rotas
 
     /**
      * Autentica o usuário
      * @param array $dados
      * @return void
      */
-
     function autenticar(array $dados)
     {
         $email = trim($dados["email"]);
@@ -67,14 +57,14 @@ class Login
      */
     public function salvarLogin(array $dados)
     {
-        
+
     }
 
 
     public function logout(array $dados)
     {
         session_start();
- 
+
         unset($_SESSION["usuario"]);
 
         // destrói a sessão
---
+++ src/Model/Database.php
@@ -0,0 +1,23 @@
+<?php
+namespace GrupoA\Supermercado\Model;
+
+class Database
+{
+    private \PDO $conexao;
+
+    public function __construct()
+    {
+        $this->conexao = new \PDO("mysql:host=localhost;dbname=dbSistema", "root", "");
+    }
+
+    public function loadUserByEmail(string $email): ?array
+    {
+        $stmt = $this->conexao->prepare("SELECT * FROM usuarios WHERE email = :email");
+        $stmt->bindParam(":email", $email);
+        $stmt->execute();
+        $usuario = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+        return $usuario ?: null;
+    }
+
+}
