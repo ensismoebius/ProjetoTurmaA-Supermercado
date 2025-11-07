342001998aa7132fdf35d5ea54b3be6577669cf4
jujuli2 <juliachaves402@gmail.com>
2025-11-07 15:25:48 -0300

    Criação do Admin.php
    com a rota para o formulario de produto implementada. #121 #122

---
+++ src/Controller/Admin.php
@@ -0,0 +1,47 @@
+<?php
+namespace GrupoA\Supermercado\Controller;
+
+use GrupoA\Supermercado\Produto;
+use GrupoA\Supermercado\Database;
+
+class Admin
+{
+    private \Twig\Environment $ambiente;
+    private \Twig\Loader\FilesystemLoader $carregador;
+
+    public function __construct()
+    {
+        // Inicia a sessão, sessao é uma forma de guardar os dados do usuario temporariamente
+        // Verifica se o usuário está logado
+        // Se não estiver, redireciona para a página de login
+        session_start();
+        if (!isset($_SESSION["usuario"])) {
+            header("Location: /login");
+            exit;
+        }
+
+        // Construtor da classe
+        $this->carregador =
+            new \Twig\Loader\FilesystemLoader("./src/View");
+
+        // Combina os dados com o template
+        $this->ambiente =
+            new \Twig\Environment($this->carregador);
+    }
+
+    /**
+     * Exibe o formulário para criação de um novo produto
+     * @param array $dados
+     * @return void
+     */
+public function formularioNovoProduto(array $dados)
+    {
+        echo $this->ambiente->render("formularioNovoProduto.html", $dados);
+    }
+
+    /**
+     * Salva um novo produto
+     * @param array $dados
+     * @return void
+     */
+}
