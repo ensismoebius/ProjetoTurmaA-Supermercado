5192f5f1a9504dc35c87d1f91ecad4c726a0b2e3
laiscalafange <laiscalafange@gmail.com>
2025-11-18 15:43:44 -0300

    pedidoItem.php
    criação do pedidoItem.php e apaguei o loadbyuser que nao estava sendo utilizado.
    resolve #163

--- src/Model/Database.php
+++ src/Model/Database.php
@@ -70,16 +70,7 @@ class Database
 
         return $stmt->execute();
     }
-    public function loadUserByName(string $nome): ?array
-{
-    $stmt = $this->conexao->prepare("SELECT * FROM usuarios WHERE nome = :nome");
-    $stmt->bindParam(":nome", $nome);
-    $stmt->execute();
 
-    $usuario = $stmt->fetch(\PDO::FETCH_ASSOC);
-
-    return $usuario ?: null;
-}
 
     // bla bla bla
 
