77467e6a41cd7c4f2f9f1b69bf3d612a37399e9b
laiscalafange <laiscalafange@gmail.com>
2025-11-28 01:03:45 -0300

    carrinho.php 2, +funções
    implementação das lógicas das funções remove, clear e view, e alterações para carregar twig

--- src/Controller/Carrinho.php
+++ src/Controller/Carrinho.php
@@ -6,15 +6,21 @@ use GrupoA\Supermercado\Service\CarrinhoUtil;
 
 class Carrinho
 {
+    private \Twig\Environment $ambiente;
+    private \Twig\Loader\FilesystemLoader $carregador;
+
     public function __construct()
     {
          Util::averigua();
          CarrinhoUtil::iniciarCarrinho();
 
+        $this->carregador = new \Twig\Loader\FilesystemLoader("./src/View/Html");
+        $this->ambiente = new \Twig\Environment($this->carregador);
     }
 
     public function add()
 {
+
     $id = $_POST['product_id'];
     $name = $_POST['name'];
     $price = floatval($_POST['price']);
@@ -33,4 +39,35 @@ class Carrinho
     header("Location: /carrinho/view");
     exit;
 }
+    public function remove()
+{
+
+    if (isset($_POST['product_id'])) {
+        $id = $_POST['product_id'];
+
+        if (isset($_SESSION['cart'][$id])) {
+            unset($_SESSION['cart'][$id]);
+        }
+    }
+
+    header("Location: /carrinho/view");
+    exit;
+}
+
+    public function clear()
+{
+    $_SESSION['cart'] = [];
+    header("Location: /carrinho/view");
+    exit;
+}
+
+public function view()
+{
+    $carrinho = $_SESSION['cart'];
+    echo $this->ambiente->render("carrinho.html", [
+        "carrinho" => $carrinho, 
+    ]);
+}
+
+
 }
